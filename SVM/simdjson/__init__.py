87384147c89ae40cd100564efcec6c12d9340396
Akutagawa <pbwfh384@gmail.com>
2019-12-16 15:00:09 +0900

    add pysimdjson

---
+++ SVM/simdjson/__init__.py
@@ -0,0 +1,14 @@
+from simdjson.csimdjson import (
+    Iterator,
+    ParsedJson,
+    loads,
+    c_loads,
+    parse_query
+)
+
+API_IMPORTS = [
+    Iterator,
+    ParsedJson,
+    loads,
+    parse_query
+]
