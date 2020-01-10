2cef7e9c26ea54fdd4b21721ffd2bcccd588447d
Akutagawa <pbwfh384@gmail.com>
2020-01-10 23:51:38 +0900

    check duplicate

--- SVM/calc.py
+++ SVM/calc.py
@@ -2,13 +2,14 @@ import argparse
 import copy
 import os
 import sys
+from collections import Counter
 
 import numpy as np
 import pytest
 
 import utils as utils
 from SVM import FeatureFucntion
-from utils import DIVIDER, parse_JSON
+from utils import DIVIDER, parse_JSON, Triplet
 
 
 def main(args):
@@ -16,8 +17,22 @@ def main(args):
     print("parsing JSON files ...")
     function_keys, programs, candidates, label_seq_dict = parse_JSON(args.json_files)
 
-    sum_edges = sum((len(program)-1) for program in programs)
-    print(sum_edges / len(programs))
+    triplet_list = []
+    for program in programs:
+        for key, obj in program.items():
+            if key == "y_names":
+                continue
+            x = obj["xName"]
+            y = obj["yName"]
+            seq = obj["sequence"]
+            key_name = Triplet(x, seq, y)
+            triplet_list.append(key_name)
+
+    c = Counter(triplet_list)
+    c_values = list(c.values())
+    print(c_values[:30])
+    c_values.sort(reverse=True)
+    print(c_values[:30])
 
 
 if __name__ == "__main__":
